27df021093ab9311415b7b30ad08d1287fff725e
study261105 <65936964+study261105@users.noreply.github.com>
2020-08-06 23:31:23 +0800

    Add files via upload
    initial commit

---
+++ src/test/java/com/MediaOperation/MediaOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MediaOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
